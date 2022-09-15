1f8e77380cddf7b5d46a73e06745050d6e971613
Ashwinipujari14 <102610850+Ashwinipujari14@users.noreply.github.com>
2022-09-16 00:27:47 +0530

    Add files via upload

---
+++ SourceCode/java/study/jparest/Controller/BookingController.java
@@ -0,0 +1,40 @@
+package study.jparest.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import study.jparest.Entity.Booking;
+import study.jparest.repository.BookingRepository;
+
+@RestController
+@RequestMapping("/cws/Booking")
+public class BookingController {
+	
+	@Autowired
+	public BookingRepository br;
+	
+	@GetMapping("/getBookings")
+	public List<Booking> getBooking()
+	{
+		return br.findAll();
+	}
+
+	@PostMapping("/BookService")
+	public String BookService(@RequestBody Booking b1)
+	{
+		Booking b = new Booking(b1.getId(),b1.getProvider_id(),b1.getCustomer_id(),b1.getService_id(),
+				b1.getService_date(),b1.getService_time(),b1.getBooking_date(),b1.isBooking_status(),
+				b1.getDiscount(),b1.getTotal_amount(),b1.getAddress(),b1.getService_contact_person());
+		br.save(b);
+		return "Service Added";
+	}
+	
+	
+}
