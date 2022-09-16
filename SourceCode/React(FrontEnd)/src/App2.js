fb1fd24e2bddc5ac0fbb536abf3b61c020467601
bhama0511 <shubhamgawande0511@gmail.com>
2022-09-16 23:16:42 +0530

    Front End

--- SourceCode/React(FrontEnd)/src/App2.js
+++ SourceCode/React(FrontEnd)/src/App2.js
@@ -1,35 +1,31 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./signupUser";
-import Appli from "./App3";
+
 import Header from "./component/Header";
 import LandingpageRouting from "./LandingpageRouting";
+import UserLogin from "./App3";
 // import { Provider } from "react-redux";
 // import store from './Store/reducer';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Routes from 'react-router-dom';
 
-const App2 = () => {
+function App2() {
     return (
         <>
             <BrowserRouter>
                 {/* <Provider store={store}> */}
                 <Routes>
-                    <Route path="/login" element={<Appli />} />
+                    <Route path="/login" element={<UserLogin />} />
                     <Route path="/" element={<LandingpageRouting />} />
                     <Route index element={<LandingpageRouting />} />
 
-                    {/* <Route path="/About" element={<About />} />
-                    <Route path="/Contact" element={<Contact />} />
-                    <Route path="/AdminLogin" element={<AdminLogin />} />
-                    <Route path="/TeacherLogin" element={<TeacherLogin />} />
-                    <Route path="/StudentLogin" element={<StudentLogin />} />
-                    <Route path="/AdminSignUp" element={<AdminSignUp />} />
-                    <Route path="/ForgotPassword" element={<ForgotPassword />} /> */}
                     <Route path="/sign-up" element={<SignUp />} />
                 </Routes>
                 {/* </Provider> */}
             </BrowserRouter>
         </>
-    )
+    );
 }
 
 export default App2;
