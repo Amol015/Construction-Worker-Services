d2a2e569ec8dba3e0ab7b6a3f72f5d034347ec5c
Ashwinipujari14 <102610850+Ashwinipujari14@users.noreply.github.com>
2022-09-18 23:32:42 +0530

    Update Customer.java

--- SourceCode/java/study/jparest/Entity/Customer.java
+++ SourceCode/java/study/jparest/Entity/Customer.java
@@ -6,6 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="customer_info_tbl")
@@ -16,22 +20,28 @@ public class Customer {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private int Id;
 	
+	@NotEmpty(message = "Field must not be null")
 	@Column(name="customer_firstname")
 	private String firstName;
+	
 	
 	@Column(name="customer_lastname")
 	private String lastName;
 	
 	@Column(name="customer_mobile")
+	@Pattern(regexp = "^$|[0-9]{10}",message = "Please input proper format")
 	private long mobile;
 	
+	@NotEmpty(message = "Field must not be null")
 	@Column(name="customer_username")
 	private String userName;
 	
+	@Size(min = 4,max = 10,message = "Password must be more than 4 and less than 10 charcters")
 	@Column(name="customer_password")
 	private String password;
 	
-	@Column(name="customer_email_id")
+	@Column(unique = true)
+	@Email(message = "Please input proper format")
 	private String emailId;
 	
 	@Column(name="customer_address")
