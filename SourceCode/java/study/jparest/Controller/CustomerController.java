6c47794b993c5b83a613037c9af115eb7893d062
Ashwinipujari14 <102610850+Ashwinipujari14@users.noreply.github.com>
2022-09-21 07:43:26 +0530

    Update CustomerController.java

--- SourceCode/java/study/jparest/Controller/CustomerController.java
+++ SourceCode/java/study/jparest/Controller/CustomerController.java
@@ -1,6 +1,7 @@
 package study.jparest.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import study.jparest.repository.CustomerRepository;
 import study.jparest.repository.ServiceProviderRepository;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/cws1")
 public class CustomerController {
@@ -36,15 +38,15 @@ public class CustomerController {
 		return "customer inserted";
 	}
 	
-	@PostMapping("/login/{eid}/{pwd}")
-	  public Customer Login(@PathVariable String eid, @PathVariable String pwd)
+	@PostMapping("/login/{userName}/{password}")
+	  public String Login(@PathVariable String userName, @PathVariable String password)
 	  {
-		Customer sp1 = cus.checkLogin(eid,pwd);
+		Customer sp1 = cus.checkLogin(userName,password);
 		if(sp1!= null)
 		{
-			return sp1;
+			return "pass";
 		}
-		return null;	  
+		return "fail";	  
 	  }
 	
 	 @PutMapping("/changePass/{email}/{oldpwd}/{newpwd}")
