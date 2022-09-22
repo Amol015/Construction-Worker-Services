87c7a6355fd3dc1c2d47a80bdf2250f5abc2d6c5
Ashwinipujari14 <102610850+Ashwinipujari14@users.noreply.github.com>
2022-09-22 22:22:17 +0530

    Update CustomerController.java

--- SourceCode/java/study/jparest/Controller/CustomerController.java
+++ SourceCode/java/study/jparest/Controller/CustomerController.java
@@ -8,63 +8,105 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import study.jparest.Entity.ServiceInfo;
-import study.jparest.repository.ServiceInfoRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import study.jparest.Entity.ServiceProvider;
+import study.jparest.repository.ServiceProviderRepository;
+import study.jparest.services.ServiceProviderServices;
 
 @RestController
-@RequestMapping("/cws3")
-public class ServiceController {
+@RequestMapping("/cws")
+public class ServiceProviderController {
 	
 	@Autowired
-	ServiceInfoRepository serrp; 
+	ServiceProviderRepository ser;
 	
-	@GetMapping("/getServices")
-	public List<ServiceInfo> getServices()
+	@GetMapping("/getAll")
+	public List<ServiceProvider> getAllServiceProviders()
 	{
-		return serrp.findAll();
+		return ser.findAll();
 	}
 	
-	@PostMapping("/addService")
-	public String addService(@RequestBody ServiceInfo se)
+	@GetMapping("/getElectricians/{role}")
+	public List<ServiceProvider> getElectricians(@PathVariable String role)
 	{
-		ServiceInfo ser = new ServiceInfo(se.getId(),se.getServiceName(),se.getServiceCharges(),se.getProviderTime(),se.getNumberofServicePerDay(),se.getProviderId());
-		serrp.save(ser);
-		return "ServiceAdded";
+		return ser.findByRole(role);
 	}
-	
-	  @PutMapping("/updateService/{id}/{serviceName}")
-	  public String updateService(@PathVariable int id ,@PathVariable String serviceName )
-	  {
-		  List<ServiceInfo> list = serrp.findAll();
-		 for(ServiceInfo service :list)
-		 {
-		  if(service.getId()==id)
-		  {
-			  service.setServiceName(serviceName);
-		  }
-		 }
-	    serrp.saveAll(list);
-	    return "change done";	 
-	  }
-	 
-	/*@DeleteMapping("/delete/{serviceName}")
-	public String deleteService(@PathVariable String serviceName )
-	{
-		serrp.deleteByName(serviceName);
-		return "deleted";
-	} */
-	
-	@DeleteMapping("/deleteById/{serviceId}")
-	public String deleteService(@PathVariable int serviceId )
-	{
-		serrp.deleteById(serviceId);
-		return "deleted";
-	}
-	
-	
 
+	  @PostMapping("/register") 
+	  public String register(@RequestBody ServiceProvider sp1)
+	  {
+		  ServiceProvider sp = new ServiceProvider(sp1.getId(),sp1.getFirstName(),sp1.getLastName(),sp1.
+	  getMobile(),sp1.getUserName(),sp1.getPassword(),sp1.getEmailId(),sp1.getCity(
+	  ),sp1.getRole(),sp1.isVerified());
+		  ser.save(sp); 
+		  return "inserted";
+	 }
+	  
+	  @PostMapping("/login/{eid}/{pwd}")
+	  public ServiceProvider Login(@PathVariable String eid, @PathVariable String pwd)
+	  {
+		ServiceProvider sp1 = ser.checkLogin(eid,pwd);
+		if(sp1!= null)
+		{
+			return sp1;
+		}
+		return null;	  
+	  }
+	  
+	  
+	  @PutMapping("/changePass/{email}/{oldpwd}/{newpwd}")
+	  public String changePassword(@PathVariable String email,@PathVariable String oldpwd,@PathVariable String newpwd)
+		{	
+			ServiceProvider u= ser.findByEmailId(email);	
+			if(u!=null)
+			{
+				if(u.getPassword().equals(oldpwd))
+				{
+					u.setPassword(newpwd);
+					System.out.println(u.getFirstName());
+					ser.save(u);
+					return "Password changed";
+				}
+			}	
+			 return "Incorrect Credentials";		
+		}
+	  
+	  
+	  @PutMapping("/update/{id}/{isVerified}")
+	  public String updateVerification(@PathVariable int id , @PathVariable boolean isVerified)
+	  {
+		  List<ServiceProvider> list= ser.findAll(); 
+			for(ServiceProvider service : list) 
+			{
+				if(service.getId()==id)
+				{ 
+					service.setVerified(isVerified);
+				}
+			}
+			ser.saveAll(list);
+			return "changes successfull :) "; 	  
+	  }
+	  
+	  @DeleteMapping("/delete/{id}")
+	  public String deletebyId(@PathVariable int id)
+	  {
+			ser.deleteById(id);
+			return "deleted";
+	  }  
+	  
+	/*	@PostMapping("/login/{email}/{pwd}")
+		public ServiceProvider checkLogin(@PathVariable String email,@PathVariable String pwd)
+		{
+			return ser.checkLoginCustomer(email, pwd);
+		} */
 }
+
+
+
+
+
+
