5bc386213055c00fc88400b1fd480335218cf805
Ashwinipujari14 <102610850+Ashwinipujari14@users.noreply.github.com>
2022-09-24 18:10:02 +0530

    Update ServiceInfoRepository.java

--- SourceCode/java/study/jparest/repository/ServiceInfoRepository.java
+++ SourceCode/java/study/jparest/repository/ServiceInfoRepository.java
@@ -1,11 +1,15 @@
 package study.jparest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import study.jparest.Entity.ServiceInfo;
 
 @Repository
 public interface ServiceInfoRepository extends JpaRepository<ServiceInfo, Integer>{
+
+	@Query(value="select * from service_info_tbl where service_name=:serviceName",nativeQuery =true)
+	void deleteByName(String serviceName);
 
 }
