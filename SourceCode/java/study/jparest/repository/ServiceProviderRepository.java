de79925b6682298859ac81223f1b9db215ccf584
Ashwinipujari14 <102610850+Ashwinipujari14@users.noreply.github.com>
2022-09-24 18:10:26 +0530

    Update ServiceProviderRepository.java

--- SourceCode/java/study/jparest/repository/ServiceProviderRepository.java
+++ SourceCode/java/study/jparest/repository/ServiceProviderRepository.java
@@ -1,11 +1,28 @@
 package study.jparest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import study.jparest.Entity.ServiceProvider;
 
 @Repository
 public interface ServiceProviderRepository extends JpaRepository<ServiceProvider,Integer>{
+
+	ServiceProvider findByEmailId(String email);
+
+	@Query(value="select * from service_provider_info_tbl where provider_email_id=:eid and provider_password=:pwd",nativeQuery = true)
+	ServiceProvider checkLogin(String eid, String pwd);
+
+	List<ServiceProvider> findByRole(String role);
+
+	//ServiceProvider addServiceProvider(ServiceProvider sp);
+
+	//ServiceProvider addServiceProvider(ServiceProvider sp);
+
+
+
 
 }
