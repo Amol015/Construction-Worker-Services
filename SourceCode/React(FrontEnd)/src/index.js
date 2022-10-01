4f41ba871b3b533653bb1371027f780dc98cf621
IamRupesh007 <rupeshkamble.1314@gmail.com>
2022-10-02 01:15:49 +0530

    Updated

--- SourceCode/React(FrontEnd)/src/index.js
+++ SourceCode/React(FrontEnd)/src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
-import Popper from 'popper.js';
+//import $ from 'jquery';
+//import Popper from 'popper.js';
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
